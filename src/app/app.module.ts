b2ebc054f7325262d5a9ff3d5fd2f3bdfdbce083
SFDC\akoudri <aminesoma@gmail.com>
2018-03-13 14:56:36 +0000

    V1 Movie LISTRESTAPI with search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { HttpClientModule } from '@angular/common/http'; 
+/*import { HttpModule } from '@angular/http';*/
+import { Http ,HttpModule} from '@angular/http';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { IconsPage } from '../pages/icons/icons';
+
+import { MovieInfoPage } from '../pages/movie-info/movie-info';
+import { MovieService } from '../services/rest/movie-service';
+ 
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    IconsPage,
+    MovieInfoPage
   ],
   imports: [
     BrowserModule,
+	HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    IconsPage,
+    MovieInfoPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    MovieService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
+  /*
+  providers: [
+	AuthGuard,
+	CookieService,
+	CompanyService,
+	PolicyService,
+	CommunicationService,
+	AuthenticationService,
+  
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]*/
 })
 export class AppModule {}
+
